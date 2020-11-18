4f2fbd898ba534c361fafa56ed71868862b2a7c8
Maksim Kalik <maxkalik@gmail.com>
2020-11-18 17:29:25 +0200

    init

---
+++ MapTest/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MapTest
+//
+//  Created by Maksim Kalik on 18/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
