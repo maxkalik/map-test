1df0a9e833b3176d84ee9d7c2f6eb967d4f19168
Maksim Kalik <maxkalik@gmail.com>
2020-11-18 20:50:14 +0200

    Map View

--- MapTest/ViewController.swift
+++ MapTest/ViewController.swift
@@ -6,27 +6,68 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var users = [User]()
+    var annotations = [MKPointAnnotation]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let service = Client(hostName: "ios-test.printful.lv", port: 6111)
-        service.start()
-        service.send(line: "AUTHORIZE maxkalik@gmail.com")
-        service.delegate = self
+        startClient()
     }
 
+    private func startClient() {
+        let client = Client(hostName: "ios-test.printful.lv", port: 6111)
+        client.start()
+        client.send(line: "AUTHORIZE maxkalik@gmail.com")
+        client.delegate = self
+    }
+    
+    private func createAnnotatinos(userList: [User]) {
+        // Walking through all users and create annotations
+        for user in userList {
+            // Before creating annotation we should prepare coordinate
+            let latitude = user.location.latitude
+            let longitude = user.location.longitude
+            let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+            // Creating annotation
+            let annotation = MKPointAnnotation()
+            // Set coordinate to annotation
+            annotation.coordinate = coordinate
+            // Append annotation to annotations
+            annotations.append(annotation)
+        }
+        mapView.addAnnotations(annotations)
+        mapView.showAnnotations(mapView.annotations, animated: true)
+    }
 }
 
 extension ViewController: ClientDelegate {
+    
+    // initial User List
     func didRecieveUserList(users: [User]) {
-        print(users)
+        createAnnotatinos(userList: users)
+        self.users = users
     }
     
+    // Updates
     func didUpdateUserCoordinates(locations: LocationUpdates) {
-        print(locations)
+        // Here we will try to figureout whose coordinate has changed
+        for updates in locations {
+            // Find a user with the same id which came from updates
+            if let index = self.users.firstIndex(where: { $0.id == updates.id }) {
+                // Checking the locatoin on difference
+                if self.users[index].location != updates.location {
+                    // Update user last location
+                    self.users[index].location = updates.location
+                    print(updates.id, updates.location)
+                }
+            }
+        }
     }
 }
+
