a288d99bd8e10d69673057e0f96c0e9b20e9a1aa
Maksim Kalik <maxkalik@gmail.com>
2020-11-18 21:54:32 +0200

    creating annotation with retrieving a placemark

--- MapTest/ViewController.swift
+++ MapTest/ViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
@@ -20,6 +21,7 @@ class ViewController: UIViewController {
         startClient()
     }
 
+    // Begin client connection
     private func startClient() {
         let client = Client(hostName: Settings.hostName, port: Settings.port)
         client.start()
@@ -27,24 +29,58 @@ class ViewController: UIViewController {
         client.delegate = self
     }
     
+    // Create annotations and add them to the map
     private func createAnnotatinos(userList: [User]) {
         // Walking through all users and create annotations
         for user in userList {
-            // Before creating annotation we should prepare coordinate
-            let latitude = user.location.latitude
-            let longitude = user.location.longitude
-            let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
-            // Creating annotation
-            let annotation = MKPointAnnotation()
-            // Set coordinate to annotation
-            annotation.coordinate = coordinate
+            let annotation = createAnnotation(for: user)
             annotations.append(annotation)
         }
         mapView.addAnnotations(annotations)
         // Show all annotations and fit them with proper zooming
         mapView.fitAnnotations()
     }
+    
+    // Create annotation fro a user
+    private func createAnnotation(for user: User) -> MKPointAnnotation {
+        // Before creating annotation we should prepare coordinate
+        let latitude = user.location.latitude
+        let longitude = user.location.longitude
+        let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        // Creating annotation
+        let annotation = MKPointAnnotation()
+        // Set coordinate to annotation
+        annotation.coordinate = coordinate
+        
+        // Look up the current location of the annotation
+        lookUpCurrentLocation(latitude: latitude, longitude: longitude) { placemark in
+            // Street name
+            let street = placemark?.thoroughfare ?? ""
+            // Building number
+            let building = placemark?.subThoroughfare ?? ""
+            print("\(street) \(building)")
+        }
+        return annotation
+    }
+    
+    // Convert a Coordinate into a Placemark
+    // This method I took from this article: https://developer.apple.com/documentation/corelocation/converting_between_coordinates_and_user-friendly_place_names
+    private func lookUpCurrentLocation(latitude: CLLocationDegrees, longitude: CLLocationDegrees, completionHandler: @escaping (CLPlacemark?) -> Void ) {
+        let geocoder = CLGeocoder()
+        geocoder.reverseGeocodeLocation(CLLocation(latitude: latitude, longitude: longitude), completionHandler: { (placemarks, error) in
+            if error == nil {
+                // Retrieve a CLPlacemark object
+                let firstLocation = placemarks?[0]
+                completionHandler(firstLocation)
+            } else {
+                // An error occurred during geocoding.
+                completionHandler(nil)
+            }
+        })
+    }
 }
+
+// MARK: - ClientDelegate
 
 extension ViewController: ClientDelegate {
     
@@ -61,6 +97,7 @@ extension ViewController: ClientDelegate {
             // Find a user with the same id which came from updates
             if let index = self.users.firstIndex(where: { $0.id == updates.id }) {
                 // Checking the locatoin on difference
+                // (in my case its only user with id 102)
                 if self.users[index].location != updates.location {
                     // Update user last location
                     self.users[index].location = updates.location
